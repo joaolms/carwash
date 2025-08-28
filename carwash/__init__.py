8a4959be60c5f3a2d085934a00479bca79dc4f0d
João Luiz Messias <joaolmss@gmail.com>
2025-08-28 10:26:27 -0300

    Add Models to database

--- carwash/__init__.py
+++ carwash/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///carwash.db"
+database = SQLAlchemy(app)
 
 from carwash import routes
---
+++ carwash/models.py
@@ -0,0 +1,25 @@
+from carwash import database
+from datetime import datetime, timezone
+
+
+class User(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String, nullable=False)
+    username = database.Column(database.String(80), nullable=False, unique=True)
+    password = database.Column(database.String(120), nullable=False)
+    phone = database.Column(database.String(20), nullable=False)
+    image = database.Column(database.String, default="user_default.png")
+    vehicles = database.relationship("vehicle", backref="user", lazy=True)
+
+class Vehicle(database.Model):
+    plate = database.Column(database.String(10), primary_key=True, unique=False, nullable=False)
+    model = database.Column(database.String(30), nullable=False)
+    year = database.Column(database.Integer)
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
+
+class Booking(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    create_at = database.Column(database.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    appointment = database.Column(database.DateTime, nullable=False)
+    vehicle_id = database.Column(database.Integer, database.ForeignKey('vehicle.plate'), nullable=False)
+    status = database.Column(database.String(20), default='Booked')
